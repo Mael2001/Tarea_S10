94bd8c37f2182d1d7c5a9ff35816b4bb5207c56b
Mael2001 <45516640+Mael2001@users.noreply.github.com>
2019-09-14 15:27:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,73 @@
 #include "nodo.h"
 #include <iostream>
 
+using namespace std;
 int main()
 {
     nodo *raiz = 0;
-    insertarAlInicio(&raiz, (char *)"PoPoPoPoncho");
-    insertarAlInicio(&raiz, (char *)"Braian");
-    insertarAlInicio(&raiz, (char *)"Anuel");
-    insertarAlFinal(&raiz, (char *)"Recio");
-    imprimir(&raiz);
-    std::cout << BuscarElemento(&raiz, (char *)"Braian");
+    char *nombre = (char *)" ";
+    int opciones{0};
+    do
+    {
+        cout << "****PROGRAMA****" << endl;
+        cout << "1. Insertar Al Inicio" << endl;
+        cout << "2. Insertar Al Final" << endl;
+        cout << "3. Ordenar" << endl;
+        cout << "4. Imprimir" << endl;
+        cout << "5. Guardar A Archivo" << endl;
+        cout << "6. Leer De Archivo" << endl;
+        cout << "7. Buscar Elemento" << endl;
+        cout << "8. Salir" << endl;
+        cin >> opciones;
+        switch (opciones)
+        {
+        case 1:
+            cout << "Ingrese Nombre A Ingresar: " << endl;
+            cin >> nombre;
+            insertarAlInicio(&raiz, nombre);
+            break;
+        case 2:
+            cout << "Ingrese Nombre A Ingresar: " << endl;
+            cin >> nombre;
+            insertarAlFinal(&raiz, nombre);
+            break;
+        case 3:
+            cout << "Ordenando" << endl;
+            Ordenar(&raiz);
+            break;
+        case 4:
+            cout << " Imprimiendo " << endl;
+            imprimir(&raiz);
+            break;
+        case 5:
+            cout << "Ponga el Nombre Del Archivo: " << endl;
+            cin >> nombre;
+            guardarArchivo(&raiz, nombre);
+            break;
+        case 6:
+            cout << "Ponga el Nombre Del Archivo: " << endl;
+            cin >> nombre;
+            cargarDesdeArchivo(nombre);
+            break;
+        case 7:
+            cout << "Ingrese Nombre A Buscar: " << endl;
+            cin >> nombre;
+            switch (BuscarElemento(&raiz, nombre))
+            {
+            case 0:
+                cout << "Nombre No Existe" << endl;
+                break;
+            case 1:
+                cout << "Nombre Existe" << endl;
+                break;
+            }
+            break;
+        case 8:
+            cout << "Saliendo Programa." << endl;
+            break;
+        default:
+            cout << "Eliga una opción del 1-8" << endl;
+            break;
+        }
+    } while (opciones != 8);
 }
